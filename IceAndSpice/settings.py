daafeb5f904acacd4e2a833591e1e7d8eb170ce0
Sahil Gupta <guptasahil670@gmail.com>
2026-07-25 03:13:59 +0000

    Add Render build script

--- IceAndSpice/settings.py
+++ IceAndSpice/settings.py
@@ -128,13 +128,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
