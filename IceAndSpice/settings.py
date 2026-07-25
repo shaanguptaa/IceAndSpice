ffe53bfebcdef729fe98894d3caff1098a010d2b
Sahil Gupta <guptasahil670@gmail.com>
2026-07-25 03:19:18 +0000

    add env vars

--- IceAndSpice/settings.py
+++ IceAndSpice/settings.py
@@ -20,13 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_%vlo3(3&^(70k54jccwfg24b^gbq@#07^r(yb75=hk@z84*r'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-n_%vlo3(3&^(70k54jccwfg24b^gbq@#07^r(yb75=hk@z84*r"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
